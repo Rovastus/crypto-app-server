2d1ef2f9fb72e6a5ea31aa627670ff71559c9e4b
Rovastus <rovastus1@gmail.com>
2023-06-28 21:14:50 +0200

    Fix lint issues + finish import of file data into database

--- src/schema/types/file.ts
+++ src/schema/types/file.ts
@@ -52,7 +52,7 @@ export function initFile(schemaBuilder: SchemaBuilderType) {
 
   schemaBuilder.mutationFields((t) => ({
     importFile: t.prismaField({
-      type: ['File'],
+      type: 'File',
       args: {
         portfolioId: t.arg({ type: 'BigInt', required: true }),
         name: t.arg.string({ required: true }),
@@ -60,7 +60,7 @@ export function initFile(schemaBuilder: SchemaBuilderType) {
       },
       resolve: async (_query, _root, args, _context, _info) => {
         const portfolio: PrismaTypes.Portfolio = await getPortfolioById(args.portfolioId, prisma);
-        const walletRecords: Array<PrismaTypes.Wallet> = await getWalletRecordsByPortfolioId(args.portfolioId, prisma);
+        const walletRecords: Array<PrismaTypes.Wallet> = await getWalletRecordsByPortfolioId(portfolio.id, prisma);
 
         const data = await processFileExport(
           args.jsonData.map((obj) => {
@@ -75,118 +75,62 @@ export function initFile(schemaBuilder: SchemaBuilderType) {
           prisma,
         );
 
-        return await prisma.file.findMany({ where: { portfolioId: args.portfolioId } });
+        const prismaPromises: PrismaTypes.PrismaPromise<PrismaTypes.File | PrismaTypes.Portfolio>[] = [];
+        prismaPromises.push(
+          prisma.file.create({
+            data: {
+              portfolio: {
+                connect: {
+                  id: args.portfolioId,
+                },
+              },
+              name: args.name,
+              jsonData: JSON.stringify(args.jsonData),
+              earns: { create: data.earns },
+              transactions: { create: data.transactions },
+            },
+          }),
+        )
+        const walletUpsert: Array<PrismaTypes.Prisma.WalletUpsertWithWhereUniqueWithoutPortfolioInput> =
+          Array.from(data.wallets, (obj) => {
+            return {
+              where: {
+                portfolioId_coin_unique: {
+                  portfolioId: portfolio.id,
+                  coin: obj.coin,
+                },
+              },
+              update: {
+                amount: obj.amount,
+                avcoFiatPerUnit: obj.avcoFiatPerUnit,
+                totalFiat: obj.totalFiat,
+              },
+              create: {
+                coin: obj.coin,
+                amount: obj.amount,
+                avcoFiatPerUnit: obj.avcoFiatPerUnit,
+                totalFiat: obj.totalFiat,
+              },
+            }
+          })
+        prismaPromises.push(
+          prisma.portfolio.update({
+            where: {
+              id: portfolio.id,
+            },
+            data: {
+              wallets: {
+                upsert: [...walletUpsert],
+              },
+              walletHistories: {
+                create: [...data.walletHistories],
+              },
+            },
+          }),
+        )
+
+        return (await prisma.$transaction([...prismaPromises]))[0] as PrismaTypes.File;
       },
     }),
   }));
 }
-
-/*
-export const Mutation = mutationField((t) => {
-  t.field('importFile', {
-    type: 'File',
-    args: {
-      portfolioId: nonNull('BigInt'),
-      name: nonNull(stringArg()),
-      jsonData: nonNull(
-        list(
-          nonNull(
-            inputObjectType({
-              name: 'ProcessFileInput',
-              definition(t) {
-                t.nonNull.string('utcTime')
-                t.nonNull.string('operation')
-                t.nonNull.string('coin')
-                t.nonNull.float('change')
-              },
-            }),
-          ),
-        ),
-      ),
-    },
-    async resolve(_root, args, ctx) {
-      const exportData: Array<ExportData> = Array.from(
-        args.jsonData,
-        (data) => {
-          return {
-            coin: data.coin,
-            change: new Decimal(data.change),
-            operation: data.operation,
-            utcTime: moment.utc(data.utcTime).toDate(),
-          }
-        },
-      )
-
-      const portfolio: PrismaTypes.Portfolio = await getPortfolioById(
-        args.portfolioId,
-        ctx.prisma,
-      )
-
-      const walletRecords: Array<PrismaTypes.Wallet> =
-        await getWalletRecordsByPortfolioId(args.portfolioId, ctx.prisma)
-
-      const data = await processExportData(
-        exportData,
-        walletRecords,
-        ctx.prisma,
-      )
-
-      const prismaPromises: PrismaTypes.PrismaPromise<any>[] = new Array()
-      prismaPromises.push(
-        ctx.prisma.file.create({
-          data: {
-            portfolio: {
-              connect: {
-                id: args.portfolioId,
-              },
-            },
-            name: args.name,
-            jsonData: JSON.stringify(args.jsonData),
-            earn: { create: data.earns },
-            transaction: { create: data.transactions },
-          },
-        }),
-      )
-      const walletUpsert: Array<PrismaTypes.Prisma.WalletUpsertWithWhereUniqueWithoutPortfolioInput> =
-        Array.from(data.wallet, (obj) => {
-          return {
-            where: {
-              portfolioId_coin_unique: {
-                portfolioId: portfolio.id,
-                coin: obj.coin,
-              },
-            },
-            update: {
-              amount: obj.amount,
-              avcoFiatPerUnit: obj.avcoFiatPerUnit,
-              totalFiat: obj.totalFiat,
-            },
-            create: {
-              coin: obj.coin,
-              amount: obj.amount,
-              avcoFiatPerUnit: obj.avcoFiatPerUnit,
-              totalFiat: obj.totalFiat,
-            },
-          }
-        })
-      prismaPromises.push(
-        ctx.prisma.portfolio.update({
-          where: {
-            id: portfolio.id,
-          },
-          data: {
-            wallet: {
-              upsert: [...walletUpsert],
-            },
-            walletHistory: {
-              create: [...data.walletHistory],
-            },
-          },
-        }),
-      )
-
-      return await ctx.prisma.$transaction([...prismaPromises])[0]
-    },
-  })
-})
-*/
